558fbb0654650066e4af29ae79e3c1f789aa27a9
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-03-06 23:19:48 +0300

    Added a new function to handle click events

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ function takeId(elt){
 $(document).ready(function() {
   $("#prac").click(function(){
     var k=takeId(hi)
+
     var newObj={
       id:n
     }
@@ -17,19 +18,77 @@ $(document).ready(function() {
 
 
 });
-$("#hi2").click(function(){
-  takeId(hi1)
+$("#prac1").click(function(){
+  takeId(hi3)
   favourites.push(n)
   alert(favourites)
 
 });
-$("#dis").click(function(){
-  $(".fet").hide();
+$("#prac2").click(function(){
+  takeId(hi4)
+  favourites.push(n)
+  alert(favourites)
+
+});
+$("#prac3").click(function(){
+  takeId(hi5)
+  favourites.push(n)
+  alert(favourites)
+
+});
+$("#prac4").click(function(){
+  takeId(hi6)
+  favourites.push(n)
+  alert(favourites)
+
+});
+$("#prac5").click(function(){
+  takeId(hi7)
+  favourites.push(n)
+  alert(favourites)
+
+});
+$("#prac6").click(function(){
+  takeId(hi8)
+  favourites.push(n)
+  alert(favourites)
+
+});
+$("#prac7").click(function(){
+  takeId(hi9)
+  favourites.push(n)
+  alert(favourites)
+
+});
+$("#prac8").click(function(){
+  takeId(hi11)
+  favourites.push(n)
+  alert(favourites)
+
+});
+$("#prac9").click(function(){
+  takeId(hi12)
+  favourites.push(n)
+  alert(favourites)
+
+});
+
+$("#showme").click(function(){
   newFavourite=favourites.map(function(favo){
-    $("#"+favo).show();
-    $(".fet").show();
+    $("."+favo).show();
+
   })
 })
+$("#houseforsale").click(function(){
+    $(".home").hide();
+    $(".house-for-sale").show();
+});
+$("#login").click(function(){
+  $("#dash").show();
+  $(".home").hide();
+  $(".house-for-sale").hide();
+})
+
 
 
 });
