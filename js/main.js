7254e13e9ff660eb240df1870a7b1511ec196426
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-03-06 20:39:06 +0300

    Added links to navigation links and added a js function total

--- js/main.js
+++ js/main.js
@@ -1,10 +1,35 @@
-$(document).ready(function() {
-  $(".zoom").mouseover(function(){
-    $(".para").fadeIn(200);
-    $(".para").animate({
-      transition:"300ms"
 
-    })
+favourites=[];
+function takeId(elt){
+   n=elt.id
+  document.getElementById('dis').innerHTML=n;
+
+}
+
+$(document).ready(function() {
+  $("#prac").click(function(){
+    var k=takeId(hi)
+    var newObj={
+      id:n
+    }
+    favourites.push(n)
+
+
+
+});
+$("#hi2").click(function(){
+  takeId(hi1)
+  favourites.push(n)
+  alert(favourites)
+
+});
+$("#dis").click(function(){
+  $(".fet").hide();
+  newFavourite=favourites.map(function(favo){
+    $("#"+favo).show();
+    $(".fet").show();
   })
+})
+
 
 });
